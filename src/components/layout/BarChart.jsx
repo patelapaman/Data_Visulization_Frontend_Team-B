e3681c038b2cc22a09cf041e8773e446833ca33b
gunjanmunjal650-sketch <gunjanmunjal650@gmail.com>
2026-07-31 00:16:51 +0530

    Update BarChart.jsx

--- src/components/layout/BarChart.jsx
+++ src/components/layout/BarChart.jsx
@@ -1,1 +1,38 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
 
+const data = [
+  { attack: "File Access", count: 198 },
+  { attack: "SQL Injection Attempt", count: 195 },
+  { attack: "Privilege Escalation", count: 191 },
+  { attack: "Brute Force", count: 189 },
+  { attack: "Login Success", count: 179 },
+];
+
+function BarChartComponent() {
+  return (
+    <div style={{ width: "100%", height: 350 }}>
+      <h2>Bar Chart (Top Attack Types)</h2>
+
+      <ResponsiveContainer>
+        <BarChart data={data}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="attack" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="count" fill="#52C41A" />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
+}
+
+export default BarChartComponent;
